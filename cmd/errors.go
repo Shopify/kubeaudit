fcc201bfca8643816d0f0310bf1e507f9f13124a
Jinank Jain <jinank94@gmail.com>
2017-10-11 21:10:22 +0200

    Support for better error handling

---
+++ cmd/errors.go
@@ -0,0 +1,15 @@
+package cmd
+
+const (
+	ESECURITY_CONTEXT_MISSING            = 1
+	ECAPABILITIES_NIL                    = 2
+	ECAPABILITIES_ADDED                  = 3
+	ENO_CAPABILITIES_DROPPED             = 4
+	EIMAGE_TAG_MISSING                   = 5
+	EINCORRECT_IMAGE                     = 6
+	EDEPRECATED_SERVICE_ACCOUNT_NAME     = 7
+	ENIL_AUTOMOUNT_SERVICE_ACCOUNT_TOKEN = 8
+	EPRIVILEGED_CONTAINER                = 9
+	ERUN_AS_ROOT                         = 10
+	ENOT_READ_ONLY_ROOT_FS               = 11
+)
