084cf66765a1c737c5d1f23e707938faf7258a0b
fedorlis <fedor_lisovskiy@zoho.com>
2017-11-06 13:01:49 -0500

    Refactor test_util into one function

--- cmd/test_util.go
+++ cmd/test_util.go
@@ -4,44 +4,40 @@ import (
 	"path/filepath"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
 var path = "../fixtures/"
 
-func runTest(t *testing.T, file string, function func(Items) []Result, errCode int) {
+func runTest(t *testing.T, file string, function interface{}, errCode int, imageStr ...string) {
 	assert := assert.New(t)
 	file = filepath.Join(path, file)
-	resources, err := getKubeResourcesManifest(file)
-	assert.Nil(err)
-	var results []Result
-	for _, resource := range resources {
-		current_results := function(resource)
-		for _, current_result := range current_results {
-			results = append(results, current_result)
+	var image imgFlags
+	switch function.(type) {
+	case (func(imgFlags, Items) []Result):
+		if len(imageStr) != 1 {
+			log.Fatal("Incorrect number of images specified")
 		}
+		image = imgFlags{img: imageStr[0]}
+		image.splitImageString()
 	}
-	var errors []int
-	for _, result := range results {
-		for _, occurrence := range result.Occurrences {
-			errors = append(errors, occurrence.id)
-		}
-	}
-	assert.Contains(errors, errCode)
-}
 
-func runImageTest(t *testing.T, file string, function func(imgFlags, Items) []Result, image_str string, errCode int) {
-	assert := assert.New(t)
-	file = filepath.Join(path, file)
-	image := imgFlags{img: image_str}
-	image.splitImageString()
 	resources, err := getKubeResourcesManifest(file)
 	assert.Nil(err)
 	var results []Result
 	for _, resource := range resources {
-		current_results := function(image, resource)
-		for _, current_result := range current_results {
-			results = append(results, current_result)
+		var currentResults []Result
+		switch f := function.(type) {
+		case (func(Items) []Result):
+			currentResults = f(resource)
+		case (func(imgFlags, Items) []Result):
+			currentResults = f(image, resource)
+		default:
+			log.Fatal("Invalid function provided")
+		}
+		for _, currentResult := range currentResults {
+			results = append(results, currentResult)
 		}
 	}
 	var errors []int
