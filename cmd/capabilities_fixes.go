f21effec64c5bc8190105767a353038128c8e640
Felix Glaser <felix.glaser@shopify.com>
2018-01-05 12:29:41 +0100

    Fix capability_fixes and write test for them

--- cmd/capabilities_fixes.go
+++ cmd/capabilities_fixes.go
@@ -5,6 +5,12 @@ import k8sRuntime "k8s.io/apimachinery/pkg/runtime"
 func fixCapabilityNotDropped(resource k8sRuntime.Object, occurrence Occurrence) k8sRuntime.Object {
 	var containers []Container
 	for _, container := range getContainers(resource) {
+		if container.SecurityContext == nil {
+			container.SecurityContext = &SecurityContext{}
+		}
+		if container.SecurityContext.Capabilities == nil {
+			container.SecurityContext.Capabilities = &Capabilities{Drop: []Capability{}, Add: []Capability{}}
+		}
 		container.SecurityContext.Capabilities.Drop = append(container.SecurityContext.Capabilities.Drop, Capability(occurrence.metadata["CapName"]))
 		containers = append(containers, container)
 	}
@@ -14,12 +20,13 @@ func fixCapabilityNotDropped(resource k8sRuntime.Object, occurrence Occurrence) 
 func fixCapabilityAdded(resource k8sRuntime.Object, occurrence Occurrence) k8sRuntime.Object {
 	var containers []Container
 	for _, container := range getContainers(resource) {
-		var add []Capability
+		add := []Capability{}
 		for _, cap := range container.SecurityContext.Capabilities.Add {
 			if string(cap) != occurrence.metadata["CapName"] {
 				add = append(add, cap)
 			}
 		}
+		container.SecurityContext.Capabilities.Add = add
 		containers = append(containers, container)
 	}
 	return setContainers(resource, containers)
--- cmd/capabilities_fixes_test.go
+++ cmd/capabilities_fixes_test.go
@@ -1,34 +1,50 @@
 package cmd
 
-//func TestFixCapabilitiesNIL(t *testing.T) {
-//	assert := assert.New(t)
-//	file := filepath.Join(path, "capabilities_nil.yml")
-//	resources, err := getKubeResourcesManifest(file)
-//	assert.Nil(err)
-//	assert.Equal(1, len(resources))
-//	resource := fixCapabilitiesNIL(resources[0])
-//	var add []Capability
-//	drop := []Capability{"AUDIT_WRITE", "CHOWN", "DAC_OVERRIDE", "FOWNER", "FSETID", "KILL", "MKNOD", "NET_BIND_SERVICE", "NET_RAW", "SETFCAP", "SETGID", "SETUID", "SETPCAP", "SYS_CHROOT"}
-//	for _, container := range getContainers(resource) {
-//		assert.Equal(add, container.SecurityContext.Capabilities.Add)
-//		assert.Equal(drop, container.SecurityContext.Capabilities.Drop)
-//	}
-//}
+import (
+	"path/filepath"
+	"testing"
 
-//func TestFixCapabilitiesNotDropeed(t *testing.T) {
-//	assert := assert.New(t)
-//	file := filepath.Join(path, "capabilities_nil.yml")
-//	resources, err := getKubeResourcesManifest(file)
-//	assert.Nil(err)
-//	assert.Equal(1, len(resources))
-//	resource := fixCapabilityNotDropped(resources[0], occurrence)
-//	for _, container := range getContainers(resource) {
-//		assert.Equal(add, container.SecurityContext.Capabilities.Add)
-//		assert.Equal(drop, container.SecurityContext.Capabilities.Drop)
-//	}
-//}
+	"github.com/stretchr/testify/assert"
+)
 
-//func TestFixCapabilityAdded(t *testing.T) {
-//fixCapabilityAdded(resource k8sRuntime.Object, occurrence Occurrence) k8sRuntime.Object {
-//	runTest(t, "capabilities_misconfigured_allow.yml", auditCapabilities, ErrorMisconfiguredKubeauditAllow)
-//}
+func TestFixCapabilitiesNotDropped(t *testing.T) {
+	assert := assert.New(t)
+	file := filepath.Join(path, "capabilities_nil.yml")
+	resources, err := getKubeResourcesManifest(file)
+	assert.Nil(err)
+	assert.Equal(1, len(resources))
+	resource := resources[0]
+	results := getResults(resources, auditCapabilities)
+	assert.Equal(1, len(results))
+	result := results[0]
+	for _, occurrence := range result.Occurrences {
+		resource = fixCapabilityNotDropped(resource, occurrence)
+	}
+	add := []Capability{}
+	drop := []Capability{"AUDIT_WRITE", "CHOWN", "DAC_OVERRIDE", "FOWNER", "FSETID", "KILL", "MKNOD", "NET_BIND_SERVICE", "NET_RAW", "SETFCAP", "SETGID", "SETUID", "SETPCAP", "SYS_CHROOT"}
+	for _, container := range getContainers(resource) {
+		assert.Equal(add, container.SecurityContext.Capabilities.Add)
+		for _, cap := range drop {
+			assert.Contains(container.SecurityContext.Capabilities.Drop, cap)
+		}
+	}
+}
+
+func TestFixCapabilityAdded(t *testing.T) {
+	assert := assert.New(t)
+	file := filepath.Join(path, "capabilities_some_allowed.yml")
+	resources, err := getKubeResourcesManifest(file)
+	assert.Nil(err)
+	assert.Equal(1, len(resources))
+	resource := resources[0]
+	results := getResults(resources, auditCapabilities)
+	assert.Equal(1, len(results))
+	result := results[0]
+	for _, occurrence := range result.Occurrences {
+		resource = fixCapabilityAdded(resource, occurrence)
+	}
+	add := []Capability{}
+	for _, container := range getContainers(resource) {
+		assert.Equal(add, container.SecurityContext.Capabilities.Add)
+	}
+}
