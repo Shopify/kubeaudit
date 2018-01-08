7986be681b2d3e66302ea83d92ad2530bcd52ef5
Felix Glaser <felix.glaser@shopify.com>
2018-01-08 12:53:09 +0100

    ASAT

--- cmd/automountServiceAccountToken_fixes_test.go
+++ cmd/automountServiceAccountToken_fixes_test.go
@@ -2,18 +2,42 @@ package cmd
 
 import "testing"
 
-func TestFixServiceAccountToken(t *testing.T) {
-	assert, resource := FixTestSetup(t, "service_account_token_true_and_no_name.yml", auditAutomountServiceAccountToken)
-	switch typ := resource.(type) {
-	case *ReplicationController:
-		assert.Equal(false, *typ.Spec.Template.Spec.AutomountServiceAccountToken)
-	}
-}
-
-func TestFixDeprecatedServiceAccount(t *testing.T) {
+func TestFixServiceAccountTokenDeprecated(t *testing.T) {
 	assert, resource := FixTestSetup(t, "service_account_token_deprecated.yml", auditAutomountServiceAccountToken)
 	switch typ := resource.(type) {
 	case *ReplicationController:
 		assert.Equal("", typ.Spec.Template.Spec.DeprecatedServiceAccount)
+	}
+}
+
+func TestFixServiceAccountTokenTrueAndNoName(t *testing.T) {
+	assert, resource := FixTestSetup(t, "service_account_token_true_and_no_name.yml", auditAutomountServiceAccountToken)
+	switch typ := resource.(type) {
+	case *ReplicationController:
+		assert.False(*typ.Spec.Template.Spec.AutomountServiceAccountToken)
+	}
+}
+
+func TestFixServiceAccountTokenNILAndNoName(t *testing.T) {
+	assert, resource := FixTestSetup(t, "service_account_token_nil_and_no_name.yml", auditAutomountServiceAccountToken)
+	switch typ := resource.(type) {
+	case *ReplicationController:
+		assert.False(*typ.Spec.Template.Spec.AutomountServiceAccountToken)
+	}
+}
+
+func TestFixServiceAccountTokenTrueAllowed(t *testing.T) {
+	assert, resource := FixTestSetup(t, "service_account_token_true_allowed.yml", auditAutomountServiceAccountToken)
+	switch typ := resource.(type) {
+	case *ReplicationController:
+		assert.True(*typ.Spec.Template.Spec.AutomountServiceAccountToken)
+	}
+}
+
+func TestFixServiceAccountTokenMisconfiguredAllow(t *testing.T) {
+	assert, resource := FixTestSetup(t, "service_account_token_misconfigured_allow.yml", auditAutomountServiceAccountToken)
+	switch typ := resource.(type) {
+	case *ReplicationController:
+		assert.False(*typ.Spec.Template.Spec.AutomountServiceAccountToken)
 	}
 }
