99ded11877176ae326c6d137ab7c93d96e7c8fed
Felix Glaser <felix.glaser@shopify.com>
2018-01-08 13:00:18 +0100

    Priv

--- cmd/privileged_fixes_test.go
+++ cmd/privileged_fixes_test.go
@@ -8,3 +8,31 @@ func TestFixPrivilegeEscalation(t *testing.T) {
 		assert.False(*container.SecurityContext.Privileged)
 	}
 }
+
+func TestFixPrivilegedNIL(t *testing.T) {
+	assert, resource := FixTestSetup(t, "privileged_nil.yml", auditPrivileged)
+	for _, container := range getContainers(resource) {
+		assert.False(*container.SecurityContext.Privileged)
+	}
+}
+
+func TestFixPrivilegedTrue(t *testing.T) {
+	assert, resource := FixTestSetup(t, "privileged_true.yml", auditPrivileged)
+	for _, container := range getContainers(resource) {
+		assert.False(*container.SecurityContext.Privileged)
+	}
+}
+
+func TestFixPrivilegedTrueAllowed(t *testing.T) {
+	assert, resource := FixTestSetup(t, "privileged_true_allowed.yml", auditPrivileged)
+	for _, container := range getContainers(resource) {
+		assert.True(*container.SecurityContext.Privileged)
+	}
+}
+
+func TestFixPrivilegedMisconfiguredAllow(t *testing.T) {
+	assert, resource := FixTestSetup(t, "privileged_misconfigured_allow.yml", auditPrivileged)
+	for _, container := range getContainers(resource) {
+		assert.False(*container.SecurityContext.Privileged)
+	}
+}
