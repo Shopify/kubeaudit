4179c50a410cb4be3e0f123a23c75f6c85e44387
Shane P. Lawrence <me@shanelawrence.info>
2018-10-04 13:52:27 -0400

    Automatically detect local/cluster mode (#113)
    * Autodetect local/cluster mode.
    
    * Initialize logging level at startup.

--- cmd/kubernetes.go
+++ cmd/kubernetes.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
-	"fmt"
+	"errors"
 	"os"
+	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
 	networking "k8s.io/api/networking/v1"
@@ -14,28 +15,42 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func kubeClientConfig(kubeconfig string) (*rest.Config, error) {
-	if kubeconfig == "" && !rootConfig.localMode {
-		fmt.Println("Running inside cluster, using the cluster config")
-		return rest.InClusterConfig()
+// ErrNoReadableKubeConfig represents any error that prevents the client from opening a kubeconfig file.
+var ErrNoReadableKubeConfig = errors.New("unable to open kubeconfig file")
+
+func kubeClientConfig() (*rest.Config, error) {
+	if rootConfig.kubeConfig != "" {
+		return kubeClientConfigLocal()
 	}
-	return clientcmd.BuildConfigFromFlags("", kubeconfig)
+
+	if config, err := rest.InClusterConfig(); err == nil {
+		log.Info("Running inside cluster, using the cluster config")
+		return config, nil
+	}
+
+	log.Info("Not running inside cluster, using local config")
+	home, ok := os.LookupEnv("HOME")
+	if !ok {
+		log.Error("Unable to load kubeconfig. No config file specified and $HOME not found.")
+		return nil, ErrNoReadableKubeConfig
+	}
+
+	rootConfig.kubeConfig = filepath.Join(home, ".kube", "config")
+	return kubeClientConfigLocal()
 }
 
-func kubeClient(kubeconfig string) (*kubernetes.Clientset, error) {
-	if rootConfig.localMode {
-		kubeconfig = os.Getenv("HOME") + "/.kube/config"
+func kubeClientConfigLocal() (*rest.Config, error) {
+	if _, err := os.Stat(rootConfig.kubeConfig); err != nil {
+		log.Errorf("Unable to load kubeconfig. Could not open file %s.", rootConfig.kubeConfig)
+		return nil, ErrNoReadableKubeConfig
 	}
-	if rootConfig.verbose == "DEBUG" {
-		log.SetLevel(log.DebugLevel)
-		log.AddHook(NewDebugHook())
-	}
-	if rootConfig.json {
-		log.SetFormatter(&log.JSONFormatter{})
-	}
-	config, err := kubeClientConfig(kubeconfig)
+	return clientcmd.BuildConfigFromFlags("", rootConfig.kubeConfig)
+}
+
+func kubeClient() (*kubernetes.Clientset, error) {
+	config, err := kubeClientConfig()
 	if err != nil {
-		log.Error(err)
+		return nil, err
 	}
 	kube, err := kubernetes.NewForConfig(config)
 	return kube, err
