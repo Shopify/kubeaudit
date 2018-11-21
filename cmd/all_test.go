7fbac30b7fcb63d9833e034e555675a7173fdc6f
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2018-11-21 07:47:07 +0100

    Add some test cases for apps/v1beta1
    Signed-off-by: Johannes M. Scheuermann <joh.scheuer@gmail.com>

--- cmd/all_test.go
+++ cmd/all_test.go
@@ -12,3 +12,12 @@ func TestAuditAll(t *testing.T) {
 	}
 	runAuditTest(t, "audit_all.yml", mergeAuditFunctions(allAuditFunctions), requiredErrors)
 }
+
+func TestAuditAllV1BETA1(t *testing.T) {
+	requiredErrors := []int{
+		ErrorAllowPrivilegeEscalationNil, ErrorAutomountServiceAccountTokenNilAndNoName, ErrorCapabilityNotDropped,
+		ErrorImageTagMissing, ErrorPrivilegedNil, ErrorReadOnlyRootFilesystemNil, ErrorResourcesLimitsNil,
+		ErrorRunAsNonRootNil, ErrorAppArmorAnnotationMissing, ErrorSeccompAnnotationMissing,
+	}
+	runAuditTest(t, "audit_all_v1beta1.yml", mergeAuditFunctions(allAuditFunctions), requiredErrors)
+}
--- cmd/autofix_test.go
+++ cmd/autofix_test.go
@@ -19,3 +19,15 @@ func TestFix(t *testing.T) {
 	assert.Nil(err)
 	assert.Nil(deep.Equal(correctlyFixedResources, fixedResources))
 }
+
+func TestFixV1Beta1(t *testing.T) {
+	file := "../fixtures/autofix_v1beta1.yml"
+	fileFixed := "../fixtures/autofix-fixed_v1beta1.yml"
+	assert := assert.New(t)
+	resources, err := getKubeResourcesManifest(file)
+	assert.Nil(err)
+	fixedResources := fix(resources)
+	correctlyFixedResources, err := getKubeResourcesManifest(fileFixed)
+	assert.Nil(err)
+	assert.Nil(deep.Equal(correctlyFixedResources, fixedResources))
+}
