3b0c33be2374743d5b76de676cc663c97380084b
Navraj Singh Chhina <navrajsingh.chhina@shopify.com>
2019-02-04 13:37:33 -0500

    resolve merge conflicts

--- cmd/autofix.go
+++ cmd/autofix.go
@@ -16,6 +16,82 @@ func getAuditFunctions() []interface{} {
 	}
 }
 
+func fixPotentialSecurityIssue(resource Resource, result Result) Resource {
+	resource = prepareResourceForFix(resource, result)
+	for _, occurrence := range result.Occurrences {
+		switch occurrence.id {
+		case ErrorAllowPrivilegeEscalationNil, ErrorAllowPrivilegeEscalationTrue:
+			resource = fixAllowPrivilegeEscalation(resource, occurrence)
+		case ErrorCapabilityNotDropped:
+			resource = fixCapabilityNotDropped(resource, occurrence)
+		case ErrorCapabilityAdded:
+			resource = fixCapabilityAdded(resource, occurrence)
+		case ErrorPrivilegedNil, ErrorPrivilegedTrue:
+			resource = fixPrivileged(resource, occurrence)
+		case ErrorReadOnlyRootFilesystemFalse, ErrorReadOnlyRootFilesystemNil:
+			resource = fixReadOnlyRootFilesystem(resource, occurrence)
+		case ErrorRunAsNonRootFalse, ErrorRunAsNonRootNil:
+			resource = fixRunAsNonRoot(resource, occurrence)
+		case ErrorServiceAccountTokenDeprecated:
+			resource = fixDeprecatedServiceAccount(resource)
+		case ErrorAutomountServiceAccountTokenTrueAndNoName, ErrorAutomountServiceAccountTokenNilAndNoName:
+			resource = fixServiceAccountToken(resource)
+		case ErrorAppArmorAnnotationMissing, ErrorAppArmorDisabled:
+			resource = fixAppArmor(resource)
+		case ErrorSeccompAnnotationMissing, ErrorSeccompDeprecated, ErrorSeccompDeprecatedPod, ErrorSeccompDisabled,
+			ErrorSeccompDisabledPod:
+			resource = fixSeccomp(resource)
+		}
+	}
+	return resource
+}
+
+func prepareResourceForFix(resource Resource, result Result) Resource {
+	needSecurityContextDefined := []int{ErrorAllowPrivilegeEscalationNil, ErrorAllowPrivilegeEscalationTrue,
+		ErrorPrivilegedNil, ErrorPrivilegedTrue, ErrorReadOnlyRootFilesystemFalse, ErrorReadOnlyRootFilesystemNil,
+		ErrorRunAsNonRootFalse, ErrorRunAsNonRootNil, ErrorServiceAccountTokenDeprecated,
+		ErrorAutomountServiceAccountTokenTrueAndNoName, ErrorAutomountServiceAccountTokenNilAndNoName,
+		ErrorCapabilityNotDropped, ErrorCapabilityAdded, ErrorMisconfiguredKubeauditAllow}
+	needCapabilitiesDefined := []int{ErrorCapabilityNotDropped, ErrorCapabilityAdded, ErrorMisconfiguredKubeauditAllow}
+
+	// Set of errors to fix
+	errors := make(map[int]bool)
+	for _, occurrence := range result.Occurrences {
+		errors[occurrence.id] = true
+	}
+
+	for _, err := range needSecurityContextDefined {
+		if _, ok := errors[err]; ok {
+			resource = fixSecurityContextNil(resource)
+			break
+		}
+	}
+
+	for _, err := range needCapabilitiesDefined {
+		if _, ok := errors[err]; ok {
+			resource = fixCapabilitiesNil(resource)
+			break
+		}
+	}
+
+	return resource
+}
+
+func fix(resources []Resource) (fixedResources []Resource) {
+	for _, resource := range resources {
+		if !IsSupportedResourceType(resource) {
+			fixedResources = append(fixedResources, resource)
+			continue
+		}
+		results := mergeAuditFunctions(getAuditFunctions())(resource)
+		for _, result := range results {
+			resource = fixPotentialSecurityIssue(resource, result)
+		}
+		fixedResources = append(fixedResources, resource)
+	}
+	return
+}
+
 // The fix function does not preserve comments (because kubernetes resources do not support comments) so we convert
 // both the original manifest file and the fixed manifest file into MapSlices (an array representation of a map which
 // preserves the order of the keys) using the Shopify/yaml fork of go-yaml/yaml (the fork adds comment support) and
