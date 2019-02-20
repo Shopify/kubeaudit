0d7ceeaa1f3d211ce1236728844a992d9be5dea0
Navraj Singh Chhina <navrajsingh.chhina@shopify.com>
2019-02-20 10:28:47 -0500

    finalize script

--- test.sh
+++ test.sh
@@ -15,19 +15,23 @@ cp -r fixtures/ fixtures_temp
 
 for f in fixtures_temp/*;
 do
-    ./kubeaudit autofix -f $f -v ERROR
+    ./kubeaudit autofix -f $f 2>> /dev/null
 done;
 
 for f in fixtures_temp/*;
-do
-    ./kubeaudit all -f $f -v ERROR 2>> result_file
+do   
+    echo $f >> result_file
+    ./kubeaudit all -f $f 2>> result_file
 done;
 
 if (( $(grep -c 'level=error' result_file) > 0 ));
     then
     echo ERROR: Regression Test Failed
+    sed -n '/^fixtures_temp/{ x; /level=error/p; d; }; /level=error/H; ${ x; /level=error/p; }' result_file
+    rm result_file
+    rm -r fixtures_temp
     exit 1 # terminate and indicate error
 fi
 
-rm -rf result_file
-rm -rf fixtures_temp
+rm result_file
+rm -r fixtures_tem
