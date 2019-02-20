71a5fd84e632a81fe567847953b22538a57f1ba0
Navraj Singh Chhina <navrajsingh.chhina@shopify.com>
2019-02-20 11:33:17 -0500

    add regression script and add error check for autofix

--- test.sh
+++ test.sh
@@ -10,3 +10,30 @@ for d in $(go list ./... | grep -v vendor); do
         rm profile.out
     fi
 done
+
+go build -o kubeaudit main.go
+
+cp -r fixtures/ fixtures_temp
+
+for f in fixtures_temp/*;
+do
+    ./kubeaudit autofix -f $f 2>> /dev/null
+done;
+
+for f in fixtures_temp/*;
+do
+    echo $f >> result_file
+    ./kubeaudit all -f $f 2>> result_file
+done;
+
+if (( $(grep -c 'level=error' result_file) > 0 ));
+    then
+    echo ERROR: Regression Test Failed
+    sed -n '/^fixtures_temp/{ x; /level=error/p; d; }; /level=error/H; ${ x; /level=error/p; }' result_file
+    rm result_file
+    rm -r fixtures_temp
+    exit 1 # terminate and indicate error
+fi
+
+rm result_file
+rm -r fixtures_temp
