7abfcb26553fedf5c3b0ee8363054eb048b67186
Navraj Singh Chhina <navrajsingh.chhina@shopify.com>
2019-02-25 17:42:13 -0500

    add TestCapabilitiesManualConfigV2 and TestCapabilitiesManualConfigV1

--- cmd/automountServiceAccountToken_fixes.go
+++ cmd/automountServiceAccountToken_fixes.go
@@ -1,9 +1,6 @@
 package cmd
 
 func fixServiceAccountToken(result *Result, resource Resource) Resource {
-	if labelExists, _ := getPodOverrideLabelReason(result, "allow-automount-service-account-token"); labelExists {
-		return resource
-	}
 	return setASAT(resource, false)
 }
 
