901919e024d04c2dd7e7636d925bffade836a4ed
Navraj Singh Chhina <navrajsingh.chhina@shopify.com>
2019-02-25 17:02:26 -0500

    remove TestPrint function

--- cmd/result_test.go
+++ cmd/result_test.go
@@ -38,22 +38,3 @@ func TestCreateFields(t *testing.T) {
 	fields := createFields(results[0], results[0].Occurrences[0])
 	assert.Equal(t, 5, len(fields))
 }
-
-// func TestPrint(t *testing.T) {
-// 	rootConfig.manifest = "../fixtures/run_as_non_root_psc_false_csc_nil_multiple_cont_v1.yml"
-// 	resources, err := getKubeResourcesManifest(rootConfig.manifest)
-// 	assert.Nil(t, err)
-// 	results := getResults(resources, auditRunAsNonRoot)
-// 	hook := logTest.NewGlobal()
-// 	results[0].Print()
-// 	assert.Equal(t, 1, len(hook.Entries))
-// 	assert.Equal(t, logrus.ErrorLevel, hook.LastEntry().Level)
-// 	hook.Reset()
-// 	rootConfig.manifest = "../fixtures/unknown_type_v1.yml"
-// 	resources, err = getKubeResourcesManifest(rootConfig.manifest)
-// 	results = getResults(resources, auditNetworkPolicies)
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, 1, len(hook.Entries))
-// 	assert.Equal(t, logrus.WarnLevel, hook.LastEntry().Level)
-// 	hook.Reset()
-// }
