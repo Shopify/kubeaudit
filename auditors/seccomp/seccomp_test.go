7500d89622ecf36dd1cbc2a2803c833e25a8c084
Sergey Chernobrivenko <sc6@metaswitch.com>
2022-09-13 11:06:35 +0100

    upd

--- auditors/seccomp/seccomp_test.go
+++ auditors/seccomp/seccomp_test.go
@@ -11,7 +11,7 @@ func TestAuditSeccomp(t *testing.T) {
 	cases := []struct {
 		file           string
 		expectedErrors []string
-		testLocal      bool
+		testLocalMode  bool
 	}{
 		{"seccomp-profile-missing.yml", []string{SeccompProfileMissing}, true},
 		{"seccomp-profile-missing-disabled-container.yml", []string{SeccompProfileMissing, SeccompDisabledContainer}, true},
@@ -29,7 +29,7 @@ func TestAuditSeccomp(t *testing.T) {
 		t.Run(tc.file, func(t *testing.T) {
 			t.Parallel()
 			test.AuditManifest(t, fixtureDir, tc.file, New(), tc.expectedErrors)
-			if tc.testLocal {
+			if tc.testLocalMode {
 				test.AuditLocal(t, fixtureDir, tc.file, New(), strings.Split(tc.file, ".")[0], tc.expectedErrors)
 			}
 		})
